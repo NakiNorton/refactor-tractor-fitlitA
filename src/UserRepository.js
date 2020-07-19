0b85fe1a8b776094ddc80e8bb8fc41a06506e656
Stephanie Norton <steph.jane.norton@gmail.com>
2020-07-18 19:06:03 -0600

    Fix spacing between methods and refactored to es6 arrow functions in interation methods

--- src/UserRepository.js
+++ src/UserRepository.js
@@ -19,7 +19,6 @@ class UserRepository {
     return communityTotal / this.users.length;
   }
 
-  
   calculateAverageSleepQuality() {
     let totalSleepQuality = this.users.reduce((sum, user) => {
       sum += user.sleepQualityAverage;
@@ -27,26 +26,30 @@ class UserRepository {
     }, 0);
     return totalSleepQuality / this.users.length;
   }
+
   calculateAverageSteps(date) {
     let allUsersStepsCount = this.users.map(user => {
       return user.activityRecord.filter(activity => {
         return activity.date === date;
       });
     })
-    let sumOfSteps = allUsersStepsCount.reduce((stepsSum, activityCollection) => {
-      activityCollection.forEach(activity => {
-        stepsSum += activity.steps
-      })
-      return stepsSum;
-    }, 0);
+    let sumOfSteps = allUsersStepsCount
+      .reduce((stepsSum, activityCollection) => {
+        activityCollection.forEach(activity => {
+          stepsSum += activity.steps
+        })
+        return stepsSum;
+      }, 0);
     return Math.round(sumOfSteps / allUsersStepsCount.length);
   }
+
   calculateAverageStairs(date) {
     let allUsersStairsCount = this.users.map(user => {
       return user.activityRecord.filter(activity => {
         return activity.date === date;
       });
     })
+
     let sumOfStairs = allUsersStairsCount.reduce((stairsSum, activityCollection) => {
       activityCollection.forEach(activity => {
         stairsSum += activity.flightsOfStairs
@@ -55,12 +58,14 @@ class UserRepository {
     }, 0);
     return Math.round(sumOfStairs / allUsersStairsCount.length);
   }
+
   calculateAverageMinutesActive(date) {
     let allUsersMinutesActiveCount = this.users.map(user => {
       return user.activityRecord.filter(activity => {
         return activity.date === date;
       });
     })
+
     let sumOfMinutesActive = allUsersMinutesActiveCount.reduce((minutesActiveSum, activityCollection) => {
       activityCollection.forEach(activity => {
         minutesActiveSum += activity.minutesActive
@@ -69,6 +74,7 @@ class UserRepository {
     }, 0);
     return Math.round(sumOfMinutesActive / allUsersMinutesActiveCount.length);
   }
+
   calculateAverageDailyWater(date) {
     let todaysDrinkers = this.users.filter(user => {
       return user.addDailyOunces(date) > 0;
@@ -78,11 +84,12 @@ class UserRepository {
     }, 0)
     return Math.floor(sumDrankOnDate / todaysDrinkers.length);
   }
+
   findBestSleepers(date) {
-    return this.users.filter(user => {
-      return user.calculateAverageQualityThisWeek(date) > 3;
-    })
+    return this.users.filter(user =>
+      user.calculateAverageQualityThisWeek(date) > 3);
   }
+
   getLongestSleepers(date) {
     return sleepData.filter(sleep => {
       return sleep.date === date;
@@ -90,12 +97,12 @@ class UserRepository {
       return b.hoursSlept - a.hoursSlept;
     })[0].userID;
   }
+
   getWorstSleepers(date) {
-    return sleepData.filter(sleep => {
-      return sleep.date === date;
-    }).sort((a, b) => {
-      return a.hoursSlept - b.hoursSlept;
-    })[0].userID;
+    return sleepData.filter(sleep => sleep.date === date)
+      .sort((a, b) => {
+        return a.hoursSlept - b.hoursSlept;
+      })[0].userID;
   }
 }
 
