55fb37c12aee78d0b4fba5f81d954345cbf90985
Stephanie Norton <steph.jane.norton@gmail.com>
2020-07-18 19:08:28 -0600

    Fix spacing between tests and commented out redundant describe function

--- test/Sleep-test.js
+++ test/Sleep-test.js
@@ -58,25 +58,32 @@ describe('Sleep', function() {
       "sleepQuality": 1.4
     }, userRepository);
   });
+
   it('should be a function', function() {
     expect(Sleep).to.be.a('function');
   });
+
   it('should be an instance of activity', function() {
     expect(sleep1).to.be.an.instanceof(Sleep);
   });
+
   it('should hold a userId', function() {
     expect(sleep2.userId).to.equal(2);
   });
+
   it('should hold a date', function() {
     expect(sleep3.date).to.equal("2019/07/17");
   });
+
   it('should hold hours slept', function() {
     expect(sleep1.hoursSlept).to.equal(6.1);
   });
+
   it('should hold sleep quality', function() {
     expect(sleep3.sleepQuality).to.equal(1.4);
   });
-  describe('sleep', function() {
+
+  // describe('sleep', function() {
     it('should update user\'s slept hours record', function() {
       expect(user1.sleepHoursRecord.length).to.equal(2);
     });
@@ -89,5 +96,5 @@ describe('Sleep', function() {
     it('should update user\'s sleep quality average', function() {
       expect(user1.sleepQualityAverage).to.equal('1.8');
     });
-  })
+  // })
 });
