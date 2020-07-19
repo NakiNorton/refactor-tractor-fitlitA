c6065e467c74edfb99b1b9bccd44a8eb7f54b2ad
Stephanie Norton <steph.jane.norton@gmail.com>
2020-07-18 18:16:15 -0600

    Fix spacing to improve readability and fixed not defined variables by declaring them

--- test/Activity-test.js
+++ test/Activity-test.js
@@ -4,9 +4,9 @@ import UserRepository from '../src/UserRepository';
 import User from '../src/User';
 
 describe('Activity', function() {
-  let activity;
-  let user;
-  let userRepository;
+  let activity1, activity2;
+  let user1, user2;
+  let mockRepository;
   beforeEach(() => {
     user1 = new User({
       'id': 1,
@@ -21,6 +21,7 @@ describe('Activity', function() {
         8
       ]
     });
+
     user2 = new User({
       "id": 2,
       "name": "Jarvis Considine",
@@ -35,63 +36,75 @@ describe('Activity', function() {
         19
       ]
     })
-    userRepository = new UserRepository();
-    userRepository.users.push(user1, user2);
+    mockRepository = new UserRepository([user1, user2]);
     activity1 = new Activity({
       "userID": 1,
       "date": "2019/06/15",
       "numSteps": 3684,
       "minutesActive": 140,
       "flightsOfStairs": 16
-    }, userRepository);
+    }, mockRepository);
     activity2 = new Activity({
       "userID": 2,
       "date": "2019/06/20",
       "numSteps": 2856,
       "minutesActive": 280,
       "flightsOfStairs": 22
-    }, userRepository);
+    }, mockRepository);
   });
+
   it('should be a function', function() {
     expect(Activity).to.be.a('function');
   });
+
   it('should be an instance of activity', function() {
     expect(activity1).to.be.an.instanceof(Activity);
   });
+
   it('should hold a userId', function() {
     expect(activity2.userId).to.equal(2);
   });
+
   it('should hold a date', function() {
     expect(activity1.date).to.equal("2019/06/15");
   });
+
   it('should hold number of steps', function() {
     expect(activity1.steps).to.equal(3684);
   });
+
   it('should hold minutes active', function() {
     expect(activity2.minutesActive).to.equal(280);
   });
+
   it('should hold flights of stairs', function() {
     expect(activity2.flightsOfStairs).to.equal(22);
   });
+
   it('should have a default value of 0 for miles walked', function() {
     expect(activity2.milesWalked).to.equal(0);
   });
+
   it('should have a default value of null for reached step goal', function() {
     expect(activity2.reachedStepGoal).to.equal(null);
   });
+
   it('doActivity should add activities to user record', function() {
     expect(user1.activityRecord.length).to.equal(1);
   });
-  it('should have a method that calculate miles walked', function() {
-    expect(activity1.calculateMiles(userRepository)).to.equal('3.0');
+
+  it.only('should have a method that calculate miles walked', function() {
+    expect(activity1.calculateMiles(mockRepository)).to.equal('3.0');
   });
+
   describe('compareStepGoal', function() {
     it('should return false if goal isn\'t met', function() {
-      activity1.compareStepGoal(userRepository);
+      activity1.compareStepGoal(mockRepository);
       expect(activity1.reachedStepGoal).to.equal(false);
     });
+
     it('should return true if goal is met', function() {
-      activity2.compareStepGoal(userRepository);
+      activity2.compareStepGoal(mockRepository);
       expect(activity2.reachedStepGoal).to.equal(true);
     });
   });
