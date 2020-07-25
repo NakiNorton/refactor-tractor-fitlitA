d032edc2580993044feb456ab864b7b60e0d061c
Stephanie Norton <steph.jane.norton@gmail.com>
2020-07-25 09:15:44 -0600

    Refactor methods to calculate and return users steps and miles for the day

--- src/ActivityRepository.js
+++ src/ActivityRepository.js
@@ -1,14 +1,53 @@
-import Activity from './Activity';
+// import Activity from './Activity';
 
 class ActivityRepository {
-  constructor(rawActivityData, todayDate) {
-    this.individualEntryRecord = [];
-    // this.activities = this.instantiateRawData(rawActivityData);
+  constructor(date) {
+    this.individualEntryRecords = [];
+    this.todaysDate = date;
+    // this.doActivity(userRepository);
+    // this.steps = data.numSteps;
+//     // this.minutesActive = data.minutesActive;
+//     // this.flightsOfStairs = data.flightsOfStairs;
+//     // this.milesWalked = 0;
+//     // this.reachedStepGoal = null;
   }
 
-  // instantiateRawData(rawActivityData) {
-  //   return rawActivityData.map((data) => new Activity(data));
+  // doActivity(userRepo) {
+  //   var activity = this;
+  //   userRepo.users.find(function (user) {
+  //     return user.id === activity.userId;
+  //   }).updateActivities(this);
   // }
+
+  getStepsForToday(date) {
+    let currentStepTotal = this.individualEntryRecords.filter(record => {
+      return record.date === date;
+    })
+    return currentStepTotal.reduce((sum, entry) => {
+      sum += entry.numSteps;
+      console.log('step sum:', sum)
+      return sum;
+    }, 0);
+  }
+
+  calculateMiles(user, date) {
+    this.individualEntryRecords.filter(record => {
+      return record.date === date; // get record for today
+    })
+    return Math.round(this.getStepsForToday(date) * user.strideLength / 5280).toFixed(1);
+  }
+
+  calculateFlightOfStairs(input) {
+    return this.flightsOfStairs += (12 * input);
+  }
+
+
+  compareStepGoal(userRepository) {
+    let userStepGoal = userRepository.users
+      .find(user => user.id === this.userId).dailyStepGoal;
+    this.reachedStepGoal = this.steps >= userStepGoal;
+  }
 }
+
 
 export default ActivityRepository;
