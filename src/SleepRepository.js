868ad566a3bf54737a3a6093acac6d89318c2046
Sean Leas <phoenix_24leas.com>
2020-07-25 16:11:57 -0600

    Add props to sleepRepo and findTodaysSleep

--- src/SleepRepository.js
+++ src/SleepRepository.js
@@ -2,16 +2,33 @@ import Sleep from "../src/Sleep";
 
 class SleepRepository {
   constructor(rawSleepData) {
-    this.individualEntryRecord = [];
-    // this.hoursSlept = [];
+    this.individualEntryRecord = ;
+    this.hoursSlept = this.findTodaysTotalSleep(todaysDate);
+    this.weeklyHoursSlept = this.getWeekAvgHours()
+    this.weeklyQualHoursSlept = this.calculateAverageHoursThisWeek(todaysDate)
   }
 
+  findUsersSleepData(rawSleepData) {
+    rawSleepData.filter(record => {
+      
+    })
+  }
   instantiateRawData(rawSleepData) {
     return rawSleepData.map(data => new Sleep(data));
   }
 
+  findTodaysTotalSleep(today) {
+    let todaysSleepEntries = this.individualEntryRecord.filter(record => {
+      return record.date === today
+    })
+    return todaysSleepEntries.reduce((sum, entry) => {
+      sum += Number(entry.hoursSlept)
+      console.log('this is somehow working i hope', sum)
+      return sum
+    })
+  }
 
-    calculateAverageHoursThisWeek(todayDate) {
+  calculateAverageHoursThisWeek(todayDate) {
     return (this.sleepHoursRecord.reduce((sum, sleepAct) => {
       let index = this.sleepHoursRecord.indexOf(this.sleepHoursRecord.find(sleep => sleep.date === todayDate));
       if (index <= this.sleepHoursRecord.indexOf(sleepAct) && this.sleepHoursRecord.indexOf(sleepAct) <= (index + 6)) {
