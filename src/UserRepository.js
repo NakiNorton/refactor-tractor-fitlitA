077fb264bbd02cb8cf6b7f5aee85f44c0771692d
Stephanie Norton <steph.jane.norton@gmail.com>
2020-07-25 16:06:56 -0600

    Refactor all users total steps function

--- src/UserRepository.js
+++ src/UserRepository.js
@@ -4,6 +4,7 @@ import User from './User';
 class UserRepository {
   constructor(rawData, todayDate) {
     this.users = this.matchDataWithUsers(rawData, todayDate); // all users should have appropriate data that matches their id
+    console.log('all users:', this.users)
   }
  
   matchDataWithUsers(rawData, todayDate) {
@@ -32,21 +33,20 @@ class UserRepository {
 
   matchActivityWithUser(users, rawActivityData) {
     users.forEach(user => {
-      user.activityInfo.individualEntryRecord = rawActivityData.filter(activityDataPoint => {
+      user.activityInfo.individualEntryRecords = rawActivityData.filter(activityDataPoint => {
         return activityDataPoint.userID === user.id;
       })
     })
   }
 
-
-  calculateCommunityAvgStepGoal() {
-    let communityStepGoals = this.users.map((user) => user.dailyStepGoal);
-    let communityTotal = communityStepGoals.reduce((sum, goal) => {
-      sum += goal;
-      return sum;
-    }, 0);
-    return communityTotal / this.users.length;
-  }
+  // calculateCommunityAvgStepGoal() {
+  //   let communityStepGoals = this.users.map((user) => user.dailyStepGoal);
+  //   let communityTotal = communityStepGoals.reduce((sum, goal) => {
+  //     sum += goal;
+  //     return sum;
+  //   }, 0);
+  //   return communityTotal / this.users.length;
+  // }
   
   // calculateAverageSleepQuality() {
   //   let totalSleepQuality = this.users.reduce((sum, user) => {
@@ -70,23 +70,25 @@ class UserRepository {
   //   let allSleepsOnDate = sleepRepository.filter((sleep) => sleep.date === date);
   //   return allSleepsOnDate ? allSleepsOnDate.sort((a, b) =>  a.hoursSlept - b.hoursSlept).shift().userID : "Data not found";
   // }
-}
 
-// calculateAverageSteps(date) {
-//   let allUsersStepsCount = this.users.map(user => {
-//     return user.activityRecord.filter(activity => {
-//       return activity.date === date;
-//     });
-//   })
-//   let sumOfSteps = allUsersStepsCount
-//     .reduce((stepsSum, activityCollection) => {
-//       activityCollection.forEach(activity => {
-//         stepsSum += activity.steps
-//       })
-//       return stepsSum;
-//     }, 0);
-//   return Math.round(sumOfSteps / allUsersStepsCount.length);
-// }
+
+  calculateAllUsersAverageSteps(date) {
+    console.log('all users:', this.users)
+    let allUsersStepsCount = this.users.map(user => {
+      return user.activityInfo.individualEntryRecords.filter(activity => {
+        return activity.date === date;
+      });
+    })
+    let sumOfSteps = allUsersStepsCount
+      .reduce((stepsSum, activityCollection) => {
+        activityCollection.forEach(activity => {
+          stepsSum += activity.numSteps
+        })
+        return stepsSum;
+      }, 0);
+    return Math.round(sumOfSteps / allUsersStepsCount.length);
+  }
+}
 
 // calculateAverageStairs(date) {
 //   let allUsersStairsCount = this.users.map(user => {
